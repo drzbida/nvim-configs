3dc7c2092400a02ab23bb16544d601845fc5bbd2
drzbida <55928036+drzbida@users.noreply.github.com>
2024-06-28 19:18:03 +0300

    adapt for nvim 0.10.0 changes

--- lua/mappings/remove.lua
+++ lua/mappings/remove.lua
@@ -4,9 +4,7 @@ local unmap = vim.keymap.del
 unmap("n", "<Leader>n") -- line numbers
 unmap("n", "<Leader>h") -- horizontal window
 unmap("n", "<Leader>v") -- vertical window
-unmap("n", "<Leader>q") -- LSP diagnostics quickfix
 unmap("n", "<Leader>th") -- Telescope themes
-unmap("n", "<Leader>lf") -- LSP floating diagnostics
 unmap("n", "<Leader>lp") -- Set log point
 unmap("n", "<M-v>") -- Toggleable vertical terminal
 unmap("n", "<M-h>") -- New horizontal terminal
--- lua/plugins/setups/venv-selector.lua
+++ lua/plugins/setups/venv-selector.lua
@@ -2,5 +2,6 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
     opts = { name = { "venv", ".venv" } },
+    branch = "regexp",
     event = "VeryLazy",
 }
