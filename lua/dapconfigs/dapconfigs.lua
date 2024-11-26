0cad27690170c4525d4f25cb9537baeee5096592
drzbida <55928036+drzbida@users.noreply.github.com>
2024-11-26 02:51:19 +0200

    add dap dotnet

--- lua/dapconfigs/dapconfigs.lua
+++ lua/dapconfigs/dapconfigs.lua
@@ -2,4 +2,8 @@ require "dapconfigs.settings.godot"
 require "dapconfigs.settings.debugpy"
 require "dapconfigs.settings.typescript"
 require "dapconfigs.settings.codelldb"
-require "dapconfigs.settings.dotnet"
+
+local ok, _ = pcall(require, "dapconfigs.dap-dotnet.dotnet")
+if not ok then
+    vim.notify("dap-dotnet not installed", vim.log.levels.WARN)
+end
